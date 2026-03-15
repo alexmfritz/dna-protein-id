554a0190c1e16ba9b33d07f9af72443c4d99b609
Alex Fritz <amfritz93@gmail.com>
2026-03-14 18:20:35 -0700

    Create intro prompting in main method

--- DNA.java
+++ DNA.java
@@ -1,3 +1,17 @@
+import java.util.*;
+import java.io.*;
+
 public class DNA {
     
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("This program reports information about DNA");
+        System.out.println("nucleotide sequences that may encode proteins.");
+
+        Scanner console = new Scanner(System.in);
+        System.out.print("Input file name? ");
+        String iFile = console.nextLine();
+        System.out.print("Output file name? ");
+        String oFile = console.nextLine();
+    }
 }
